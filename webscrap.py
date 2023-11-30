20b3328c33acc23a59cc8aa81d1906d0346bbd38
Yago Escalfoni <Yago.escalfoni@outlook.com>
2023-11-29 21:17:41 -0300

    leitura das pasginas de tenis

--- webscrap.py
+++ webscrap.py
@@ -2,31 +2,47 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from bs4 import BeautifulSoup
+import time
 
 # Caminho para o seu arquivo do WebDriver do Chrome
 webdriver_path = 'D:\Workspace\Web Scraping\chrome-win64\chrome.exe'
 
-url = 'https://www.nike.com.br/nav/esportes/casual/genero/masculino/idade/adulto/tamanho/44/tipodeproduto/calcados'
-
 # Configurações do navegador
 chrome_options = Options()
-chrome_options.add_argument("--headless")  # Execução em modo headless (sem interface gráfica)
 chrome_options.add_argument("--disable-blink-features=AutomationControlled")  # Desativa detecção de automação
 chrome_options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36")
+# Configurações adicionais para simular comportamento humano
+chrome_options.add_argument("--window-size=1920,1080")  # Tamanho da janela do navegador
+chrome_options.add_argument("--start-maximized")  # Maximiza a janela do navegador
 
 driver = webdriver.Chrome(options=chrome_options)
 
-driver.get(url)
-html_source_selenium = driver.page_source
+stopToken = True;
+iterador = 1;
 
-# Parseia o HTML com BeautifulSoup e utiliza o método prettify() para formatar
-soup = BeautifulSoup(html_source_selenium, 'html.parser')
-pretty_html = soup.prettify()
+while True:
+    url = 'https://www.nike.com.br/nav/esportes/casual/genero/masculino/idade/adulto/tamanho/44/tipodeproduto/calcados?page={}'.format(iterador)
+    driver.get(url)
+    time.sleep(5)
+    # Procura grid de sneakers
+    items = driver.find_elements(By.XPATH, "/html/body/div[4]/div[2]/div[2]/div/div/div[3]/div[2]/div[2]/div[1]")
 
-# Salva o HTML formatado em um arquivo de texto
-with open('html_nike2.html', 'w', encoding='utf-8') as file:
-    file.write(pretty_html)
+    if not items:
+        break
+    else:
+        # Pega o HTML da página
+        html_source_selenium = driver.page_source
+
+        # Parseia o HTML e utiliza o método prettify para formatar
+        soup = BeautifulSoup(html_source_selenium, 'html.parser')
+        pretty_html = soup.prettify()
+
+        # Salva o HTML formatado em um arquivo de texto
+        path = 'D:\Workspace\SneakerService\Pages\SneakerPage{}'.format(iterador)
+
+        with open(path, 'w', encoding='utf-8') as file:
+            file.write(pretty_html)
+        print(f"Arquivo da página {iterador} salvo com sucesso");
+        iterador += 1
 
 driver.quit()
-
-print("HTML formatado salvo com sucesso no arquivo 'html_output.txt'")
